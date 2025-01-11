6a4bf1d33d92ad7d5308baa6d53815ca24551043
starvieree <ainurreza90788@gmail.com>
2025-01-10 20:28:50 -0800

    Setup Splash Screen

--- lib/utils/constants/image_string.dart
+++ lib/utils/constants/image_string.dart
@@ -1,10 +1,10 @@
 class TImages {
   
   // -- App Logos
-  static const String darkAppLogo = "";
-  static const String lightAppLogo = "";
+  static const String darkAppLogo = "assets/logos/slack.png";
+  static const String lightAppLogo = "assets/logos/slack.png";
 
   // -- Social Logos
-  static const String google = "";
-  static const String facebook = "";
+  static const String google = "assets/logos/google.png";
+  static const String facebook = "assets/logos/facebook.png";
 }
