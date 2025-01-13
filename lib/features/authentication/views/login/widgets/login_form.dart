19c0bc525635a129990b16ee7898ad510410b4c6
starvieree <ainurreza90788@gmail.com>
2025-01-12 22:25:01 -0800

    Bottom Navigation Bar

--- lib/features/authentication/views/login/widgets/login_form.dart
+++ lib/features/authentication/views/login/widgets/login_form.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/features/authentication/views/password_configuration/forget_passsword.dart';
 import 'package:ecommerce_app/features/authentication/views/signup/signup.dart';
+import 'package:ecommerce_app/navigation_menu.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
 import 'package:ecommerce_app/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
@@ -62,7 +63,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const NavigationMenu()),
                 child: const Text(TTexts.signIn)
               ),
             ),
